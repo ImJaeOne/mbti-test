a26d0a95a393848d9fa330f32b6aeec70dc39b0a
ImJaeOne <dlawi0108@naver.com>
2025-02-20 23:21:35 +0900

    chore: 필요 없는 console 삭제

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,6 @@ import { authAxios } from "./axiosInstance";
 export const authRegister = async (userData) => {
   try {
     const response = await authAxios.post("/register", userData);
-    console.log("회원가입 성공:", response);
     return response.data;
   } catch (error) {
     console.error("회원가입 실패:", error.response.data.message);
@@ -14,7 +13,6 @@ export const authRegister = async (userData) => {
 export const authLogin = async (userData) => {
   try {
     const response = await authAxios.post("/login", userData);
-    console.log("로그인 성공:", response);
     localStorage.setItem("accessToken", response.data.accessToken);
     return response.data;
   } catch (error) {
