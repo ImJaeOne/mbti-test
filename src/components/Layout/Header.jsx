c11cf7de8eced3cdcdeaa14f444c965f90d1622f
ImJaeOne <dlawi0108@naver.com>
2025-02-20 23:25:40 +0900

    feat: auth 상태에 따른 Header 구현

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,32 +1,40 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useAuthStore from "../../zustand/authStore";
 
 const Header = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const { accessToken, logout } = useAuthStore((state) => state);
 
-  const handleLoginToggleBtn = () => {
-    setIsLogin(!isLogin);
+  const handleLogout = () => {
+    logout();
+    alert("로그아웃되었습니다.");
   };
+
   return (
-    <header className="fixed top-0 w-[100%] h-20 px-12 flex justify-between items-center text-lg bg-slate-300">
+    <header className="fixed top-0 w-[100%] h-20 px-12 flex justify-between items-center text-lg bg-red-300 text-white">
       <div className="">
         <Link to="/">홈</Link>
       </div>
-      <button onClick={handleLoginToggleBtn}>
-        {isLogin ? "로그아웃" : "로그인"}
-      </button>
-      <div className="flex gap-3">
-        {isLogin ? (
+      <div className="flex gap-3 items-center">
+        {accessToken ? (
           <>
             <Link to="/profile">프로필</Link>
             <Link to="/test">테스트</Link>
             <Link to="/result">결과보기</Link>
+            <button
+              className="bg-red-400 hover:bg-red-500 px-2 py-1 rounded-lg transition-colors"
+              onClick={handleLogout}
+            >
+              로그아웃
+            </button>
           </>
         ) : (
           <>
-            {" "}
-            <Link to="/login">로그인</Link>
-            <Link to="/signup">회원가입</Link>
+            <Link
+              className="bg-red-400 hover:bg-red-500 px-2 py-1 rounded-lg transition-colors"
+              to="/login"
+            >
+              로그인
+            </Link>
           </>
         )}
       </div>
