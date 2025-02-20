9f52e128ef123d7711479b63a4306b9f543d1063
ImJaeOne <dlawi0108@naver.com>
2025-02-20 17:06:59 +0900

    design: SignContainer flex

--- src/components/Sign/SignContainer.jsx
+++ src/components/Sign/SignContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SignContainer = ({ children }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
+    <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full flex flex-col items-center">
       {children}
     </div>
   );
