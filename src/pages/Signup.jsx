5f9ac51df635f8ea85dca308b5c8dc3b392b1df2
ImJaeOne <dlawi0108@naver.com>
2025-02-20 17:05:30 +0900

    design: Signup css

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import SignContainer from "../components/Sign/SignContainer";
+import SignInput from "../components/Sign/SignInput";
+import SignInputWrapper from "../components/Sign/SignInputWrapper";
+import CommonBtn from "../components/common/CommonBtn";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <SignContainer>
+      <div className="text-3xl font-bold text-primary-color mb-6">회원가입</div>
+      <SignInputWrapper>
+        <SignInput type="email" placeholder="아이디" />
+        <SignInput type="password" placeholder="비밀번호" />
+        <SignInput type="text" placeholder="닉네임" />
+        <CommonBtn text="로그인" />
+      </SignInputWrapper>
+      <div className="flex gap-2">
+        이미 계정이 있으신가요?
+        <Link to="/login" className="text-red-300">
+          로그인
+        </Link>
+      </div>
+    </SignContainer>
+  );
+};
 
-export default Signup
+export default Signup;
