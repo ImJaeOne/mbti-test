0a2b291f92b3b31780afa2fdbeae100767916624
ImJaeOne <dlawi0108@naver.com>
2025-02-22 23:14:00 +0900

    refactor: API url env 관리

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-const AUTH_API_URL = "https://www.nbcamp-react-auth.link";
-
-const TEST_API_URL = "http://localhost:5000/testResults";
-
 export const authAxios = axios.create({
-  baseURL: AUTH_API_URL,
+  baseURL: import.meta.env.VITE_AUTH_API_URL,
 });
 
 export const testAxios = axios.create({
-  baseURL: TEST_API_URL,
+  baseURL: import.meta.env.VITE_TEST_API_URL,
 });
