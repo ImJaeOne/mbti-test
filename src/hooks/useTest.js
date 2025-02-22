59ff2e73654e1f94161af475870c9cf7f241df1b
ImJaeOne <dlawi0108@naver.com>
2025-02-22 14:00:15 +0900

    fix: useQuery 반환값 없을 시 null로 고정

--- src/hooks/useTest.js
+++ src/hooks/useTest.js
@@ -12,7 +12,7 @@ export const useTestResult = (userId) => {
     queryFn: async () => {
       try {
         const testResults = await getTestResults();
-        return testResults.find((result) => result.userId === userId);
+        return testResults.find((result) => result.userId === userId) || null;
       } catch (error) {
         console.error("테스트 결과 불러오는데 문제가 발생했습니다.", error);
         return null;
@@ -28,7 +28,7 @@ export const useTestResults = () => {
     queryFn: async () => {
       try {
         const testResults = await getTestResults();
-        return testResults;
+        return testResults || null;
       } catch (error) {
         console.error("테스트 결과 불러오는데 문제가 발생했습니다.", error);
         return null;
