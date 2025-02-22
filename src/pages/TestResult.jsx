40e101b93a61aae17f07d3bef299114040219082
ImJaeOne <dlawi0108@naver.com>
2025-02-22 16:24:54 +0900

    design: TestResult 페이지 padding 설정

--- src/pages/TestResult.jsx
+++ src/pages/TestResult.jsx
@@ -2,7 +2,7 @@ import TestResultList from "../components/TestResults/TestResultList";
 
 const TestResult = () => {
   return (
-    <div className=" bg-white flex flex-col items-center">
+    <div className=" bg-white flex flex-col items-center p-6">
       <h1 className="text-3xl font-bold text-primary-color mb-6">
         다른 사람들의 MBTI는? 
       </h1>
