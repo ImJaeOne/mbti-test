3e5405a681de6ba56277dd75944e38373c9062a3
ImJaeOne <dlawi0108@naver.com>
2025-02-24 14:07:47 +0900

    refactor: ExpireTimer 필요없는 상태 제거 및 formatTime 분리

--- src/components/Layout/ExpireTimer.jsx
+++ src/components/Layout/ExpireTimer.jsx
@@ -1,48 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import useAuthStore from "../../zustand/authStore";
 import useAuth from "../../hooks/useAuth";
+import { formatTime } from "../../utils/formatExpiretTime";
 
 const ExpireTimer = () => {
   useAuth();
 
-  const { accessToken, expiresInTime, setExpiresInTime } = useAuthStore(
-    (state) => state
-  );
-  const [expiresIn, setExpiresIn] = useState(expiresInTime);
+  const { accessToken, expiresInTime, setExpiresInTime } = useAuthStore();
 
   useEffect(() => {
-    if (!expiresIn) {
-      setExpiresIn(expiresInTime);
-    }
+    if (!expiresInTime) return;
 
     const interval = setInterval(() => {
-      setExpiresIn((prev) => {
-        if (prev <= 0) {
-          clearInterval(interval);
-          return 0;
-        }
-        return prev - 1;
-      });
+      setExpiresInTime(expiresInTime - 1);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [accessToken]);
+  }, [accessToken, expiresInTime]);
 
-  useEffect(() => {
-    if (expiresIn !== expiresInTime) {
-      setExpiresInTime(expiresIn);
-    }
-  }, [expiresIn]);
-
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${String(minutes).padStart(2, "0")}:${String(
-      remainingSeconds
-    ).padStart(2, "0")}`;
-  };
   return (
-    <span className="text-sm hidden md:block">{formatTime(expiresIn)}</span>
+    <span className="text-sm hidden md:block">{formatTime(expiresInTime)}</span>
   );
 };
 
