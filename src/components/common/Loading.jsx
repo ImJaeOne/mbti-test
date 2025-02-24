08c32836c47fadf2d117c26fe8026ea00cffa949
ImJaeOne <dlawi0108@naver.com>
2025-02-24 16:44:22 +0900

    design: bg-color 변경

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+    <div className="w-full h-screen flex items-center justify-center bg-white">
       <div className="flex flex-col items-center">
         <div className="w-16 h-16 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></div>
         <p className="mt-4 text-lg font-semibold text-gray-700">
